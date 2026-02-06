096b22b3793bbe2f8e5a2f434f024c8137ddfe45
Trickylady <evi.muselle@gmail.com>
2026-02-06 04:42:22 +0100

    Player & Enemy made | player movement finished

--- scripts/level.gd
+++ scripts/level.gd
@@ -3,6 +3,9 @@ class_name Level
 
 @onready var tile_map: TileMapLayer = $TileMap
 
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+
 func swap_tiles ():
 	var cells1 = tile_map.get_used_cells_by_id(1, Vector2i.ZERO, 1)
 	var cells2 = tile_map.get_used_cells_by_id(1, Vector2i.ZERO, 2)
@@ -11,6 +14,6 @@ func swap_tiles ():
 	for cell in cells2:
 		tile_map.set_cell(cell,1, Vector2i.ZERO,1)
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
-		swap_tiles()
+#func _input(event: InputEvent) -> void:
+	#if event.is_action_pressed("ui_accept"):
+		#swap_tiles()
--- scripts/manager.gd
+++ scripts/manager.gd
@@ -4,8 +4,32 @@ const TILE_SIZE = Vector2(80,80)
 
 enum Difficulty{NORMAL, HARD}
 
+const levels = [
+	"res://scenes/level_1.tscn", 
+	"res://scenes/level_2.tscn", 
+	"res://scenes/level_3.tscn", 
+	"res://scenes/level_4.tscn", 
+	"res://scenes/level_5.tscn"
+]
+
+var current_level: int = 0
 var mode: Difficulty
 
+func _ready() -> void:
+	Input.set_custom_mouse_cursor(preload("res://images/menu/cursor.png"))
+
 func start_game(difficulty: Difficulty):
+	current_level = 0
 	mode = difficulty
-	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+	go_to_next_level()
+
+func go_to_main_menu():
+	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
+
+func go_to_next_level() -> void:
+	current_level +=1
+	if current_level >= levels.size():
+		print("wtf")
+		return
+	var level_path: String = levels[current_level -1]
+	get_tree().change_scene_to_file(level_path)
