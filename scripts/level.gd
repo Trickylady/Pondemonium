f53dd4363330b13be87686289bb677d7c1f85ea5
Trickylady <evi.muselle@gmail.com>
2026-02-09 16:38:17 +0100

    Frog spawns added

--- scripts/level.gd
+++ scripts/level.gd
@@ -3,6 +3,7 @@ class_name Level
 
 @onready var tile_map: TileMapLayer = $TileMap
 @onready var bullets: Node2D = $bullets
+@onready var enemies: Node2D = $enemies
 
 func _init() -> void:
 	Manager.level = self
@@ -18,6 +19,21 @@ func swap_tiles ():
 	for cell in cells2:
 		tile_map.set_cell(cell,1, Vector2i.ZERO,1)
 
-#func _input(event: InputEvent) -> void:
-	#if event.is_action_pressed("ui_accept"):
-		#swap_tiles()
+func spawn_frog() -> void:
+	var tile_bubbles: Array[TileBubble] = get_tile_bubbles()
+	var picked: TileBubble = tile_bubbles.pick_random()
+	var new_frog: Enemy = preload("res://scenes/enemy.tscn").instantiate()
+	new_frog.position = picked.position
+	picked.destroy()
+	enemies.add_child(new_frog)
+
+func get_tile_bubbles() -> Array[TileBubble]:
+	var result: Array[TileBubble] = []
+	for child: Node2D in tile_map.get_children():
+		if child is TileBubble:
+			result.append(child)
+	return result
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		spawn_frog()
