bf7691cd2d8bc0bf23884b8af22a07e261bc0efc
Trickylady <evi.muselle@gmail.com>
2026-02-10 01:18:25 +0100

    Score calculations on tiles added

--- scripts/cat_projectile.gd
+++ scripts/cat_projectile.gd
@@ -19,8 +19,10 @@ func _physics_process(delta: float) -> void:
 	position += direction * delta * speed
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.has_method("destroy"):
+	if body is Enemy:
 		body.destroy()
+	elif body is TileBubble:
+		body.destroy(true)
 	explode()
 
 func explode():
--- scripts/tilebubble.gd
+++ scripts/tilebubble.gd
@@ -1,16 +1,17 @@
 extends StaticBody2D
 class_name TileBubble
 
-func destroy():
+func destroy(with_points: bool = false):
 	$Tilebubble.play("pop")
 	$popsound.play()
 	$CollisionShape2D.set_deferred("disabled", true)
 	
-	var point_label: ScoreLabel = preload("res://scenes/scorelabel.tscn").instantiate()
-	point_label.position = position
-	point_label.points_display = -20
-	point_label.target_pos = Manager.uigame.bucket.global_position
-	Manager.level.score_labels.add_child(point_label)
+	if with_points:
+		var point_label: ScoreLabel = preload("res://scenes/scorelabel.tscn").instantiate()
+		point_label.position = position
+		point_label.points_display = -20
+		point_label.target_pos = Manager.uigame.bucket.global_position
+		Manager.level.score_labels.add_child(point_label)
 
 func _on_tilebubble_animation_finished() -> void:
 	queue_free()
